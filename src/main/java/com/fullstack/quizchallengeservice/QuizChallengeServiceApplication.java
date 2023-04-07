4bf155b3412e996afcf0ca087ae16ccc5d276029
name <75804600+ionutdinco@users.noreply.github.com>
2023-04-07 16:58:11 +0300

    Initial Commit

---
+++ src/main/java/com/fullstack/quizchallengeservice/QuizChallengeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.quizchallengeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizChallengeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizChallengeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/quizchallengeservice/QuizChallengeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.quizchallengeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizChallengeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
